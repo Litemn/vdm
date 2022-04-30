189285624de3a277981866546b72e425016b00dc
Sergey Boishtyan <ssboishtyan@avito.ru>
2022-04-30 11:54:50 +0300

    Add DeviceConfig.kt, Location.kt VirtualDevice.kt

--- vdm/src/main/kotlin/com/malinskiy/vdm/AndroidVirtualDeviceManager.kt
+++ vdm/src/main/kotlin/com/malinskiy/vdm/AndroidVirtualDeviceManager.kt
@@ -4,25 +4,30 @@
 
 package com.malinskiy.vdm
 
+import com.malinskiy.vdm.device.DeviceConfig
+import com.malinskiy.vdm.device.Location
+import com.malinskiy.vdm.device.VirtualDevice
+
 class AndroidVirtualDeviceManager : VirtualDeviceManager {
 
-    override fun browse() {
+    override fun browse(): List<VirtualDevice> {
         TODO("Not yet implemented")
     }
 
-    override fun read() {
+    override fun read(location: Location): VirtualDevice {
         TODO("Not yet implemented")
     }
 
-    override fun edit() {
+    override fun update(location: Location, updatedConfig: DeviceConfig): VirtualDevice {
         TODO("Not yet implemented")
     }
 
-    override fun add() {
+    override fun add(config: DeviceConfig): VirtualDevice {
         TODO("Not yet implemented")
     }
 
-    override fun delete() {
+    override fun delete(location: Location) {
         TODO("Not yet implemented")
     }
+
 }
--- vdm/src/main/kotlin/com/malinskiy/vdm/VirtualDeviceManager.kt
+++ vdm/src/main/kotlin/com/malinskiy/vdm/VirtualDeviceManager.kt
@@ -4,10 +4,15 @@
 
 package com.malinskiy.vdm
 
+import com.malinskiy.vdm.device.DeviceConfig
+import com.malinskiy.vdm.device.Location
+import com.malinskiy.vdm.device.VirtualDevice
+
 interface VirtualDeviceManager {
-    fun browse()
-    fun read()
-    fun edit()
-    fun add()
-    fun delete()
+    fun browse(): List<VirtualDevice>
+    fun read(location: Location): VirtualDevice
+    fun update(location: Location, updatedConfig: DeviceConfig): VirtualDevice
+    fun add(config: DeviceConfig): VirtualDevice
+    fun delete(location: Location)
+    fun start(location: Location, options: StartupOptions)
 }
